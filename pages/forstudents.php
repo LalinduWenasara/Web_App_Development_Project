e77b6caf05a16ba2ea1d64ff79431eb53299d83b
Lalindu Wenasara <lalinduwenu@gmail.com>
2019-12-22 08:01:42 +0530

    Create connection
    retrive data

---
+++ pages/forstudents.php
@@ -0,0 +1,12 @@
+<?php
+  $servername="localhost";
+  $susername="root";
+  $spassword="";
+  $sdbname="nsbmedge";
+
+// Create connection
+$con = mysqli_connect("$servername", "$susername","$spassword", "$sdbname");
+
+
+$conn->close();
+?> 
