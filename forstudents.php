1549d68dcfe7c77eb044659563aa46aa9d617e7e
Lalindu Wenasara <lalinduwenu@gmail.com>
2019-12-28 23:31:55 +0530

    update3

--- forstudents.php
+++ forstudents.php
@@ -83,11 +83,11 @@ if (mysqli_num_rows($result) > 0)
    {
       ?>
       <tr class="trclass1" >
-      <td > 
+      <td class="menutd1" > 
       <img src="uploads/<?php echo $row['fimage']; ?>"  class="img1">
       </td ><td class="td22">
       <?php
-      echo " " . $row["fname"]. "</td> <td>  " . $row["fdescription"]. "</td>";
+      echo " " . $row["fname"]. "</td> <td class="menutd2">  " . $row["fdescription"]. "</td>";
     
       echo "<td> " . $row["favailability"]. "</td> <td>  " . $row["price"]. "</td>";
       }
